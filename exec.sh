2fdc0555dedcddb308a2bb73d0b317d4a2fd6811
Kampylafkas Ioannis <sdi9500781@di.uoa.gr>
2017-03-18 19:32:41 +0200

    Added productions for basic integer arithmetic.

---
+++ exec.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+java -jar target/grc-0.0.1-SNAPSHOT.jar $*
--- src/main/java/k31/grc/App.java
+++ src/main/java/k31/grc/App.java
@@ -1,13 +1,17 @@
 package k31.grc;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.PushbackReader;
 
 import k31.grc.lexer.Lexer;
 import k31.grc.lexer.LexerException;
 import k31.grc.node.EOF;
+import k31.grc.node.Start;
 import k31.grc.node.Token;
+import k31.grc.parser.Parser;
+import k31.grc.parser.ParserException;
 
 /**
  * Hello world!
@@ -18,11 +22,27 @@ public class App {
 	public static void main(String[] args) {
 
 		// System.out.println("Hello World!");
-
 		System.out.print("hello\n");
+		System.out.println(args[0]);
 
-		Lexer lexer = new Lexer(new PushbackReader(new InputStreamReader(System.in), 4096));
+		
+		FileReader fr;
 
+		try {
+
+			fr = new FileReader(args[0]);
+
+		} catch (FileNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+
+			return;
+		}
+
+		
+		Lexer lexer = new Lexer(new PushbackReader(fr, 4096));
+
+		
 		Token token;
 
 		try {
@@ -36,6 +56,43 @@ public class App {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		
+		try {
+
+			fr = new FileReader(args[0]);
+
+		} catch (FileNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+
+			return;
+		}
+
+		
+		lexer = new Lexer(new PushbackReader(fr, 4096));
+		
+		
+		Parser parser = new Parser(lexer);
+
+		Start s;
+
+		try {
+
+			s = parser.parse();
+
+			System.out.println("success");
+
+		} catch (ParserException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (LexerException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
