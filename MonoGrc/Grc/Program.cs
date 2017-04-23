b377cc6c9916a988e15b0386a34bf2482ebe82b5
Kampylafkas Ioannis <sdi9500781@di.uoa.gr>
2017-04-23 13:11:24 +0300

    - Switched Java version to 1.7 - Added initial .Net and Mono projects referencing the ikvm produced dll

---
+++ MonoGrc/Grc/Program.cs
@@ -0,0 +1,1 @@
+../../NetGrc/Grc/Program.cs
---
+++ NetGrc/Grc/Program.cs
@@ -0,0 +1,16 @@
+﻿using k31.grc.ast.node.aux;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
