1f04a371a3d5e3c9397555f80bf93eea4b060d93
Kampylafkas Ioannis <sdi9500781@di.uoa.gr>
2017-05-01 14:15:46 +0300

    Moved parser test code to separate subdirectory.

--- scripts/allexec.sh
+++ scripts/allexec.sh
@@ -2,4 +2,4 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-(cd ${DIR}/..; for d in code/*; do echo $d; ./scripts/fexec.sh $d; done)
+(cd ${DIR}/../..; for d in code/*; do echo $d; ./scripts/fexec.sh $d; done)
